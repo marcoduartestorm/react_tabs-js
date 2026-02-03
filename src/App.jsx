6412afcea7f58ff72ade619f0b18386c7f464aec
marcoduartestorm <marcosbottocomp@gmail.com>
2026-02-02 22:29:36 -0300

    Solution

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 import './App.scss';
+import { Tabs } from './components/Tabs';
 
 export const tabs = [
   { id: 'tab-1', title: 'Tab 1', content: 'Some text 1' },
@@ -12,37 +13,11 @@ export const tabs = [
 export const App = () => {
   const [activeTabId, setActiveTabId] = useState(tabs[0].id);
 
+  const onTabSelected = (id) => {
+    setActiveTabId(tabs[Number(id[4]) - 1].id);
+  };
+
   return (
-    <div className="section">
-      <h1 className="title">{`Selected tab is Tab ${activeTabId[4]}`}</h1>
-
-      <div data-cy="TabsComponent">
-        <div className="tabs is-boxed">
-          <ul>
-            {tabs.map(tab => {
-              return (
-                <li
-                  className={activeTabId === tab.id ? 'is-active' : ''}
-                  data-cy="Tab"
-                >
-                  <a
-                   onClick={() => {
-                    setActiveTabId(tab.id);
-                   }}
-                   href={`#${tab.id}`}
-                   data-cy="TabLink">
-                    {`Tab ${tab.id[4]}`}
-                  </a>
-                </li>
-              );
-            })}
-          </ul>
-        </div>
-
-        <div className="block" data-cy="TabContent">
-          {`Some text ${activeTabId[4]}`}
-        </div>
-      </div>
-    </div>
+    <Tabs activeTabIdProps={activeTabId} onTabSelectedProps={onTabSelected} tabsProps={tabs}/>
   );
 };
--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -1,1 +1,38 @@
-export const Tabs = () => {};
+export const Tabs = ({activeTabIdProps, onTabSelectedProps, tabsProps}) => {
+
+  return (
+      <div className="section">
+        <h1 className="title">{`Selected tab is Tab ${activeTabIdProps[4]}`}</h1>
+
+        <div data-cy="TabsComponent">
+          <div className="tabs is-boxed">
+            <ul>
+              {tabsProps.map(tab => {
+                return (
+                  <li
+                    className={activeTabIdProps === tab.id ? 'is-active' : ''}
+                    data-cy="Tab"
+                    key={tab.id}
+                  >
+                    <a
+                      onClick={() => {
+                        onTabSelectedProps(tab.id);
+                      }}
+                      href={`#${tab.id}`}
+                      data-cy="TabLink"
+                    >
+                      {`Tab ${tab.id[4]}`}
+                    </a>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+
+          <div className="block" data-cy="TabContent">
+            {`Some text ${activeTabIdProps[4]}`}
+          </div>
+        </div>
+      </div>
+  );
+};
